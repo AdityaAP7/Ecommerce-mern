491249d3a213c6bae5a69c86647894f5d9f5ca4a
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-22 23:31:49 +0530

    Update store.js

--- Frontend/src/app/store.js
+++ Frontend/src/app/store.js
@@ -1,4 +1,4 @@
-qqqqqimport { configureStore, createReducer } from '@reduxjs/toolkit';
+qqqqqqimport { configureStore, createReducer } from '@reduxjs/toolkit';
 import productReducer from '../features/product/productSlice';
 import authReducer from '../features/auth/authSlice';
 import cartReducer from '../features/cart/cartSlice';
