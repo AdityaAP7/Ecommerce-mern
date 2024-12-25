f68b6bcb838c0c538b39aa8a3bca5f39c4e7f5d0
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-25 23:19:10 +0530

    Update ProtectedAdmin.js

--- Frontend/src/features/auth/components/ProtectedAdmin.js
+++ Frontend/src/features/auth/components/ProtectedAdmin.js
@@ -20,9 +20,6 @@ import { selectUserInfo } from '../../user/userSlice';
 2
 22
 
-2
-2
-
 function ProtectedAdmin({ children }) {
   const user = useSelector(selectLoggedInUser);
   const userInfo = useSelector(selectUserInfo)
