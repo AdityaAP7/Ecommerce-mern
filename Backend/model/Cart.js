fa30d2639ac4e49770bc1b365105a16ee4c7fc57
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-28 23:30:18 +0530

    Update Cart.js

--- Backend/model/Cart.js
+++ Backend/model/Cart.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-
+1
 const cartSchema = new Schema({
     quantity: { type : Number, required: true},
     product: { type: Schema.Types.ObjectId, ref: 'Product', required: true},
