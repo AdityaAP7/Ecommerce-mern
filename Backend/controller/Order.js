06d7438ef4bc27c5b62cedae8ef0fd33115306c4
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-28 23:36:51 +0530

    Update Order.js

--- Backend/controller/Order.js
+++ Backend/controller/Order.js
@@ -1,4 +1,4 @@
-qqconst { Order } = require("../model/Order");
+qqqconst { Order } = require("../model/Order");
 const { Product } = require("../model/Product");
 const { User } = require("../model/User");
 const { sendMail, invoiceTemplate } = require("../services/common");
