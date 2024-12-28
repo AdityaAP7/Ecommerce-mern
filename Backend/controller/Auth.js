507ad69334a275ff2f00068ec328277a8339c6c1
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-28 23:34:15 +0530

    Update Auth.js

--- Backend/controller/Auth.js
+++ Backend/controller/Auth.js
@@ -1,4 +1,4 @@
-const { User } = require('../model/User');
+1const { User } = require('../model/User');
 const crypto = require('crypto');
 const { sanitizeUser } = require('../services/common');
 const jwt = require('jsonwebtoken');
