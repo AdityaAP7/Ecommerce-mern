738711871ff7b05dcf154f2bc220faf2f7ce2af0
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-31 23:48:39 +0530

    Update Product.js

--- Backend/model/Product.js
+++ Backend/model/Product.js
@@ -1,4 +1,4 @@
-aaqqq111111111qqqqqqqqqqqqqqqqq1321const mongoose = require('mongoose');
+aaaqqq111111111qqqqqqqqqqqqqqqqq1321const mongoose = require('mongoose');
 const {Schema} = mongoose; mongoose = require('mongoose');
 const {Schema} = mongoose; mongoose = require('mongoose');
 const {Schema} = mongoose;
