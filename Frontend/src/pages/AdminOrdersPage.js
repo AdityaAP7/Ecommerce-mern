962abdb94cb00377a68b2bf0dec303d3db89dfd3
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-03 22:54:13 +0530

    Update AdminOrdersPage.js

--- Frontend/src/pages/AdminOrdersPage.js
+++ Frontend/src/pages/AdminOrdersPage.js
@@ -1,4 +1,4 @@
-q11qqadmin/components/AdminOrders";
+qq11qqadmin/components/AdminOrders";
 import NavBar admin/components/AdminOrders";
 import NavBar from "../features/navbar/Navbar";
  "../features/navbar/Navbar";
