a76d34987f0a8fb8222d022185e88d5b596a4399
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-03 22:52:19 +0530

    Update cartAPI.js

--- Frontend/src/features/cart/cartAPI.js
+++ Frontend/src/features/cart/cartAPI.js
@@ -1,4 +1,4 @@
-111export function addToCart(item) {
+1111export function addToCart(item) {
   return new Promise(async (resolve) => {
     const response = await fetch('/cart', {
       method: 'POST',
