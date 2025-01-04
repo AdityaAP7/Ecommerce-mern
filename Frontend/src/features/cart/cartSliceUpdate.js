2ce645c4bfeeefe6a90480362a033156ab75c64f
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-04 22:37:46 +0530

    Update cartSliceUpdate.js

--- Frontend/src/features/cart/cartSliceUpdate.js
+++ Frontend/src/features/cart/cartSliceUpdate.js
@@ -1,2 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { addToCart, deleteItemFromCart, fetchItemsByUserId, resetCart, updateCart } from './cartAPI';
+const initialState = {
+  status: 'idle',
+  items: [],
+  cartLoaded: false
+};
